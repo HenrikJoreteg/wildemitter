cf6362d21ae0302a283b7a4e1cc201291b66f5e8
MK <kohenkatz@gmail.com>
2015-10-14 14:37:36 -0400

    add tests from HenrikJoreteg/wildemitter#8

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ var apple = new Fruit('apple'),
     orange = new Fruit('orange');
 
 exports['Make sure wildcard handlers work'] = function (test) {
-    var count = 0, 
+    var count = 0,
         cb = function () {
             return function () {count++}
         };
@@ -119,6 +119,81 @@ exports['Test on and off'] = function (test) {
     //ensure callbacks array is removed entirely
     test.ok(orange.callbacks.test == null);
     test.ok(orange.callbacks.test2 == null);
+
+    test.done();
+};
+
+exports['Mixin to constructor'] = function (test) {
+    function Vegetable(name) {
+        this.name = name;
+    }
+
+    Emitter.mixin(Vegetable);
+    console.log(Vegetable);
+
+    Vegetable.prototype.test = function () {
+        this.emit('test', this.name);
+    };
+
+    // set up a test vegetable
+    var asparagus = new Vegetable('asparagus');
+    var count, cb1, cb2;
+
+    count = 0;
+    cb1 = function () {
+        count++;
+    };
+    cb2 = function () {
+        count++;
+    };
+
+    lettuce.on('test', cb1);
+    lettuce.on('test2', cb2);
+    lettuce.test();
+
+    test.equal(count, 1);
+
+    lettuce.off('test', cb1);
+    lettuce.test();
+    test.equal(count, 1);
+
+    lettuce.emit('test2');
+    test.equal(count, 2);
+
+    test.done();
+};
+
+
+exports['Mixin to plain javascript objects'] = function (test) {
+    var potato = {};
+    Emitter.mixin(potato);
+
+    potato.test = function () {
+        this.emit('test', this.name);
+    };
+
+    var count, cb1, cb2;
+
+    count = 0;
+    cb1 = function () {
+        count++;
+    };
+    cb2 = function () {
+        count++;
+    };
+
+    potato.on('test', cb1);
+    potato.on('test2', cb2);
+    potato.test();
+
+    test.equal(count, 1);
+
+    potato.off('test', cb1);
+    potato.test();
+    test.equal(count, 1);
+
+    potato.emit('test2');
+    test.equal(count, 2);
 
     test.done();
 };
