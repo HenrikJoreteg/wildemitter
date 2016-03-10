d0165743a28402ae8d34b3b8c332a47d27ca37c1
MK <kohenkatz@gmail.com>
2016-03-10 16:27:13 -0500

    don't build bare file anymore

--- build.js
+++ build.js
@@ -1,17 +1,15 @@
-// Creates the andbang.js source from the provided API
+// Creates the wildemitter.js source from the provided API
 // specification.
 
 /*global __dirname*/
 var fs = require('fs'),
     fileName = 'wildemitter.js',
-    bareFileName = 'wildemitter-bare.js',
     template = fs.readFileSync(__dirname + "/src/template.js").toString(),
     context = {
         emitter: fs.readFileSync(__dirname + "/src/core.js").toString(),
         intro: fs.readFileSync(__dirname + "/src/intro.js").toString()
     },
     outputPath = __dirname + '/' + fileName,
-    bareOutputPath = __dirname + '/' + bareFileName;
 
 var mustache = require('mustache'),
     yetify = require('yetify'),
@@ -24,7 +22,4 @@ console.log('\n' + yetify.logo());
 fs.writeFileSync(outputPath, code, 'utf-8');
 console.log(fileName.bold + ' file built.'.grey);
 
-fs.writeFileSync(bareOutputPath, context.emitter, 'utf-8');
-
-console.log(bareFileName.bold + ' file built.'.grey + '\n');
 process.exit(0);
