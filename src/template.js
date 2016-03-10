2915f58869ccfc29c94e48ab6f2092a4ceb68795
MK <kohenkatz@gmail.com>
2016-03-10 16:24:39 -0500

    forgot to change copy-paste

--- src/template.js
+++ src/template.js
@@ -11,7 +11,7 @@
         module.exports = factory();
     } else {
         // Browser globals (root is window)
-        root.returnExports = factory();
+        root.WildEmitter = factory();
   }
 }(this, function () {
 
